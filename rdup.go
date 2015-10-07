c80df0a780c0df7832c2aeaa7e18cd89ddf2b961
Kim YongBin <kybinz@gmail.com>
2015-10-07 15:13:11 +0900

    add '-n' argument for line buffering

--- rdup.go
+++ rdup.go
@@ -2,18 +2,37 @@ package main
 
 import (
     "os"
+    "flag"
     "bufio"
     "fmt"
 )
 
+var nprev int
+
+func init() {
+    flag.IntVar(&nprev, "n", 1, "how many lines to remember")
+}
+
 func main() {
+    flag.Parse()
+
     scanner := bufio.NewScanner(os.Stdin)
-    prev := ""
+    prevs := make([]string, 0)
     for scanner.Scan() {
         in := scanner.Text()
-        if in != prev {
+        match := false
+        for _, p := range prevs {
+            if in == p {
+                match = true
+                break
+            }
+        }
+        if !match {
             fmt.Println(in)
-            prev = in
+            prevs = append(prevs, in)
+            if len(prevs) > nprev {
+                prevs = prevs[1:]
+            }
         }
     }
     if err := scanner.Err(); err != nil {
